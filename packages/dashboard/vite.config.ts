a32522c1daf5487443469a68d5a8c52077792ee4
XFlightTogether <nextech.tw@gmail.com>
2026-09-06 20:43:57 +0800

    Update vite.config.ts

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/v1': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
   },
   build: {
     outDir: 'dist',
